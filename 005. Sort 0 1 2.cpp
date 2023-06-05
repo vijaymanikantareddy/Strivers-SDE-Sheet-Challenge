6929bff47df30354da9c2b5df94dabe809947bd9
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-06-05 21:31:42 +0530

    Create 005. Sort 0 1 2.cpp

---
+++ 005. Sort 0 1 2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   int i = 0, j = 0, k = n-1;
+   while(j <= k){
+      if(arr[j] == 0){
+         swap(arr[i], arr[j]);
+         i++;
+         j++;
+      }else if(arr[j] == 2){
+         swap(arr[j], arr[k]);
+         k--;
+      }else{
+         j++;
+      }
+   }
+}
